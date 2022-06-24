f91270299aad2aba8854d7405a46c19af413d985
Big Sexy <bigsexy@MacBook-Pro-3.local>
2022-06-24 17:45:18 -0600

    added anime-apparel files

---
+++ src/routes/home/home.component.jsx
@@ -0,0 +1,17 @@
+import {Outlet} from 'react-router-dom';
+import Directory from '../../components/directory/directory.component';
+import Footer from '../../components/footer/footer.component';
+
+
+const Home = () =>  {
+  return (
+  <div>
+      <Directory />
+      <Outlet/>
+  </div>
+
+
+  );
+};
+
+export default Home;
